fe35786057cd0863d92802e9b67a2bbb19e05aea
Andrey <andrey.develop.a@gmail.com>
2023-12-10 01:21:41 +0700

    JWT TOKEN

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Post\IndexController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('refresh', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('me', [App\Http\Controllers\AuthController::class, 'login']);
+
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Post', 'middleware' => 'jwt.auth'], function (){
+    Route::get('/posts', IndexController::class);
 });
